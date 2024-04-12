37fdbe8ee6496d1e0ebe5aae8191dc745a67c5b4
zenginhayri3 <zenginhayri3@gmail.com>
2024-04-12 13:51:03 +0300

    SıgnalR consume

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -1,16 +1,45 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.BusinessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
 
 namespace SignalRApi.Hubs
 {
     public class SignalRHub:Hub
     {
-        SignalRContext context = new SignalRContext();
-        
-        public async Task SendCategoryCount()
+        private readonly ICategoryService _categoryService;
+        private readonly IProductService _productService;
+
+        public SignalRHub(ICategoryService categoryService,IProductService productService)
         {
-            var value = context.Categorys.Count();
+            _categoryService = categoryService;
+            _productService = productService;
+        }
+
+        public async Task SendStatistic()
+        {
+            var value = _categoryService.TCategoryCount();
             await Clients.All.SendAsync("ReceiveCategoryCount", value);
+
+            var value2 = _productService.TProductCount();
+            await Clients.All.SendAsync("ReceiveProductCount", value2);
+
+            var value3 = _categoryService.TActiveCategoryCount();
+            await Clients.All.SendAsync("ReceiveActiveCategoryCount", value3);
+
+            var value4 = _categoryService.TPassiveCategoryCount();
+            await Clients.All.SendAsync("ReceivePassiveCategoryCount", value4);
+
+            var value5 = _productService.TProductCountByCategoryNameHamburger();
+            await Clients.All.SendAsync("ReceiveProductCountByCategoryNameHamburger", value5);
+
+            var value6 = _productService.TProductCountByCategoryNameDrink();
+            await Clients.All.SendAsync("ReceiveProductCountByCategoryNameDrink", value6);
+
+            var value7 = _productService.TProductPriceAvg();
+            await Clients.All.SendAsync("ReceiveProductPriveAvg", value7.ToString("0.00") + "£");
+
+            var value8 = _productService.TProductNameByMaxPrice();
+            await Clients.All.SendAsync("ReceiveProductNameByMaxPrice", value8);
         }
     }
 }
